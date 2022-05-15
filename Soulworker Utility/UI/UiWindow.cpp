8cee12a36881628d36ce77bc175b230a499b0b03
fearek <46384967+fearek@users.noreply.github.com>
2022-05-15 17:55:56 +0200

    read all font glyphs

--- Soulworker Utility/UI/UiWindow.cpp
+++ Soulworker Utility/UI/UiWindow.cpp
@@ -173,13 +173,12 @@ BOOL UiWindow::SetFontList() {
 		char fontPath[MAX_BUFFER_LENGTH] = { 0 };
 		strcat_s(fontPath, path);
 		strcat_s(fontPath, fd.name);
-
-#ifdef SERVER_KOREA
-		io.Fonts->AddFontFromFileTTF(fontPath, 32, &config, io.Fonts->GetGlyphRangesKorean());
-#endif
-#ifdef SERVER_STEAM
-		io.Fonts->AddFontFromFileTTF(fontPath, 32, &config, io.Fonts->GetGlyphRangesChineseFull());
-#endif
+//#ifdef SERVER_KOREA
+		//io.Fonts->AddFontFromFileTTF(fontPath, 32, &config, io.Fonts->GetGlyphRangesKorean());
+//#endif
+//#ifdef SERVER_STEAM
+		io.Fonts->AddFontFromFileTTF(fontPath, 32, &config, io.Fonts->GetGlyphRangesChineseAndKoreaFull());
+//#endif
 #ifdef SERVER_JAPAN
 		/* GetGlyphRangesJapanese() doesn't contain CJK Ideograms, so it can't display "肆" at "猫刀・肆の型：月斬" */
 		// io.Fonts->AddFontFromFileTTF(fontPath, 32, &config, io.Fonts->GetGlyphRangesJapanese());
--- Soulworker Utility/main.cpp
+++ Soulworker Utility/main.cpp
@@ -35,7 +35,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	else {
 		LogInstance.WriteLog(const_cast<LPTSTR>(_T("Init UIWINDOW Failed")));
 	}
-
 	MiniDump::End();
 }
 
